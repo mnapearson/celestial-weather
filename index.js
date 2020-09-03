429f0cef4c65613a1d86af6610bbebcb01a79140
mnapearson <marratoon@gmail.com>
2020-09-03 17:41:51 +0200

    add welcome css and button function

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("welcome-button").addEventListener(
+  "click",
+  function () {
+    document.getElementById("welcome").hidden = true;
+    document.getElementById("homepage").hidden = false;
+  },
+  false
+);
