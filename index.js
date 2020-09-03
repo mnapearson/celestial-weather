130be3f28e5adf5e5931272c8aa14bcee7757100
mnapearson <marratoon@gmail.com>
2020-09-03 21:44:32 +0200

    add weather, oracle, cloud image

--- index.js
+++ index.js
@@ -6,3 +6,41 @@ document.getElementById("welcome-button").addEventListener(
   },
   false
 );
+
+document.getElementById("magic-button").addEventListener(
+  "click",
+  function () {
+    document.getElementById("magic-answer").hidden = false;
+  },
+  false
+);
+
+const urlAnswer = "http://localhost:3000/answers/random";
+const urlMoon = "http://wttr.in/leipzig?format=%25m";
+const urlWeather = "http://wttr.in/leipzig?format=2&%25=C";
+
+fetch(urlAnswer)
+  .then((response) => {
+    return response.json();
+  })
+  .then((answers) => {
+    document.getElementById("magic-answer").textContent = JSON.stringify(
+      answers.answer
+    );
+  });
+
+fetch(urlMoon)
+  .then((response) => {
+    return response.text();
+  })
+  .then((data) => {
+    document.getElementById("moon-phase").textContent = data;
+  });
+
+fetch(urlWeather)
+  .then((response) => {
+    return response.text();
+  })
+  .then((data) => {
+    document.getElementById("current-weather").textContent = data;
+  });
