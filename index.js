0f6f42dbf9b20e113d3dd7861409d435ac407f3c
mnapearson <marratoon@gmail.com>
2020-09-04 16:30:33 +0200

    add secure to wttr

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ document.getElementById("magic-button").addEventListener(
 );
 
 const urlAnswer = "https://crystal-ball-express.herokuapp.com/answers/random";
-const urlMoon = "http://wttr.in/leipzig?format=%25m";
-const urlWeather = "http://wttr.in/leipzig?format=2&%25=C";
+const urlMoon = "https://wttr.in/leipzig?format=%25m";
+const urlWeather = "https://wttr.in/leipzig?format=2&%25=C";
 const urlApod =
   "https://api.nasa.gov/planetary/apod?api_key=z73R3rNe2S3kyi1Edljco6bUuJ8HwFtnrBh8uFgb";
 
